f909a366705e3ec833cbb8994f29b1a995ac2e29
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-07-01 17:11:27 +0200

    test

--- srcs/raytracer/load_image.c
+++ srcs/raytracer/load_image.c
@@ -6,7 +6,7 @@
 /*   By: plamtenz <plamtenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 18:53:16 by plamtenz          #+#    #+#             */
-/*   Updated: 2020/07/01 15:17:40 by plamtenz         ###   ########.fr       */
+/*   Updated: 2020/07/01 17:10:59 by plamtenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <hooks.h>
 #include <stdlib.h>
 #include <aux.h>
+#include <X11/X.h>
 
 t_scene      *copy_structure(t_scene *s)
 {
@@ -77,10 +78,10 @@ bool 				load_image(t_scene *s)
     if (!(s->flags & SAVE))
     {
         (void)mlx_put_image_to_window(s->image->mlx, s->image->win, s->image->img, 0, 0);
-    	//mlx_key_hook(s->image->win, key_hook, s);
+    	mlx_key_hook(s->image->win, key_hook, s);
     	//mlx_mouse_hook(s->image->win, mouse_hook, s);
     	//mlx_hook(s->image->win, 6, (1L << 6), motion_hook, s);
-    	mlx_hook(s->image->win, 17, 0L, motion_end, s);
+    	mlx_hook(s->image->win, DestroyNotify, StructureNotifyMask, motion_end, s);
         mlx_loop(s->image->mlx);
     }
     else
